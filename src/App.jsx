929f4875c7cb05e36f63d454c17ea9fb99012224
slideshuffle <52997518+slideshuffle@users.noreply.github.com>
2022-06-13 17:40:44 +0900

    jsx修正

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
+import ColorfulMessage from "./components/ColorfulMessage";
 
 const App = () => {
-  const onClickButton = () => alert();
-  const contentStyle = {
-    color: "blue",
-    fontSize: "18px"
+  const [num, setNum] = useState(0);
+  const [faceShowFlag, setFaceShowFlag] = useState(false);
+
+  const onClickCountUp = () => {
+    setNum(num + 1);
   };
+
   return (
     <>
       <h1 style={{ color: "red" }}>こんにちは！</h1>
-      <p style={contentStyle}>お元気ですか？</p>
-      <button onClick={onClickButton}>ボタン</button>
+      <ColorfulMessage color="blue">お元気ですか？</ColorfulMessage>
+      <ColorfulMessage color="pink">元気です！</ColorfulMessage>
+      <button onClick={onClickCountUp}>カウントアップ</button>
+      <br />
+      <button>on/off</button>
+      <p>{num}</p>
+      {/* {console.log(faceShowFlag);} */}
+      {/* {<p>console.log(faceShowFlag);</p>} */}
+      {faceShowFlag && <p>(^_-)-☆</p>}
     </>
   );
 };
