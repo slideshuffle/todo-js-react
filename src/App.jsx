f8995d485af13eebe063c4a60a9ec5170fd6e7e1
slideshuffle <52997518+slideshuffle@users.noreply.github.com>
2022-06-14 10:37:26 +0900

    jsx更新

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,29 @@
-import React, { useState } from "react";
+/* eslint react-hooks/exhaustive-deps:off */
+import React, { useEffect, useState } from "react";
 import ColorfulMessage from "./components/ColorfulMessage";
 
 const App = () => {
+  console.log("最初");
   const [num, setNum] = useState(0);
   const [faceShowFlag, setFaceShowFlag] = useState(false);
 
   const onClickCountUp = () => {
     setNum(num + 1);
   };
+  const onClickSwitchShowFlag = () => {
+    setFaceShowFlag(!faceShowFlag);
+  };
+
+  useEffect(() => {
+    if (num > 0) {
+      if (num % 3 === 0) {
+        faceShowFlag || setFaceShowFlag(true);
+      } else {
+        faceShowFlag && setFaceShowFlag(false);
+      }
+    }
+    //eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [num]);
 
   return (
     <>
@@ -16,10 +32,8 @@ const App = () => {
       <ColorfulMessage color="pink">元気です！</ColorfulMessage>
       <button onClick={onClickCountUp}>カウントアップ</button>
       <br />
-      <button>on/off</button>
+      <button onClick={onClickSwitchShowFlag}>on/off</button>
       <p>{num}</p>
-      {/* {console.log(faceShowFlag);} */}
-      {/* {<p>console.log(faceShowFlag);</p>} */}
       {faceShowFlag && <p>(^_-)-☆</p>}
     </>
   );
